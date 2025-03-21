596a78d068274862c297e3f9beb9c1b71c519d2a
safwanv06 <safwanv06official@gmail.com>
2025-03-22 00:29:17 +0530

    Meeting scheduler application

--- lib/constants/image_data/image_data.dart
+++ lib/constants/image_data/image_data.dart
@@ -2,17 +2,11 @@
 ///Non icon assets
 class ImageData {
   static const String _basePath = "assets/images";
-
-  String cmfLogo = "$_basePath/cmf_logo.png";
-  String farmersMessage = "$_basePath/farmers_message.png";
-  String profileBackground = "$_basePath/profile_background.jpg";
 }
 
 ///svg icon assets
 class IconsData {
   static const String _basePath = "assets/icons";
-
-  String iconNoImage = "$_basePath/icon_no_image.svg";
 
 }
 
